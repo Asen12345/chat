bbba1e3a899f2a4692e80fb4d8b220b24aab87d2
macbook <playegrovoy@gmail.com>
2024-11-30 12:03:20 +0300

    Initial Commit

---
+++ SpbAIHelper/SpbAIHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpbAIHelperApp.swift
+//  SpbAIHelper
+//
+//  Created by MacBook on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpbAIHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
